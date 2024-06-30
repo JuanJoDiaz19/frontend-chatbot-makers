291ea90b29feee4249711cb894b634ee2b2fd682
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-06-30 11:31:21 -0500

    feat: adding the deploy of the backedn

--- makersbot/src/api/ProductsApiService.js
+++ makersbot/src/api/ProductsApiService.js
@@ -2,7 +2,10 @@ import { apiBackend } from "./BackendApi"
 
 // export const retrieveHelloWorldBean = () => apiBackend.get('/hello-world-bean')
 
-export const retrieveAllCategoriesWithProducts = () => apiBackend.get(`categories`);
+export const retrieveAllCategoriesWithProducts = () => apiBackend.get(`categories`,);
+
+export const deleteOneProduct = (id) => apiBackend.delete(`products/${id}`);
+
 
 // export const executeBasicAuthenticationService = (token) => apiBackend.get(`/basic-auth`, {
 //     headers: {
